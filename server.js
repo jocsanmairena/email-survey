95c41a669a473ac5f8f252801dac1d009869955f
jocsanmairena <jocsan.mairena@gmail.com>
2019-07-10 23:58:32 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+
+//app represents the underlyiong running express server.
+//The express server has route handlers associated with it. Like "get"
+app.get('/', (req, res) => {
+    res.send({
+        hi: 'there'
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000);
