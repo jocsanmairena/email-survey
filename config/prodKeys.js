7806e737006b7c28068f7c9d71759b5244de5512
jocsanmairena <jocsan.mairena@gmail.com>
2019-08-11 01:10:38 -0400

    First commit: environment variables dev and prod

---
+++ config/prodKeys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID :'902960002479-05vm5tj1sa5jshtkbjpnj9iq5u6374ql.apps.googleusercontent.com',
+    googleClientSecret: 'pvBXCT5hmcB2wxQlucv9Bykf',
+    mongoURI: 'mongodb+srv://dev:dev@cluster0-xaaje.mongodb.net/test?retryWrites=true&w=majority',
+    cookieKey: 'jdb38onf27804^**#Khdjhdjfjhh!@&746k+-e389ihhjdfdk'
+};
